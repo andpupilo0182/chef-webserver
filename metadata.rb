449106556c5ff745dd357102335be2e0996e4216
Andre Ferreira <andreluis0182@gmail.com>
2019-06-14 10:57:00 -0300

    Version update

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'seunome@seudominio'
 license 'All Rights Reserved'
 description 'Servidor Apache'
 long_description 'Configuracao e Instalacao do servidor Apache'
-version '1.1.1'
+version '1.1.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 source_url "https://github.com/andreluis0182/webserver-andreluis0182"
